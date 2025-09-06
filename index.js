8562c9ee1ac48d2ed3cee9c4b3bf181e9f5b58d6
Vishesh Kudva <vishesh.kudva@outlook.com>
2025-09-06 18:59:57 +1200

    Fixed navbar css

--- index.js
+++ index.js
@@ -1,66 +1,60 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-
-
-
-
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 const orderLimit = 2;
 const banLimit = 5;
 
 const menu = [
-    {
-        name: 'cookies',
-        price: 2.5,
+  {
+    name: "Cookies",
+    price: 2.5,
+  },
+  {
+    name: "Brownies",
+    price: 2,
+    custom: {
+      mnms: 25,
+      oreos: 25,
+      sprinkles: 25,
+      marshmallows: 25,
+      mnms: 25,
+      oreos: 25,
+      sprinkles: 25,
+      marshmallows: 25,
+      sauces: {
+        choco: 50,
+        caramel: 50,
+        strawberry: 50,
+      },
     },
-    {
-        name: 'brownies',
-        price: 2,
-        custom: {
-            mnms: 25,
-            oreos: 25,
-            sprinkles: 25,
-            marshmallows: 25,
-            mnms: 25,
-            oreos: 25,
-            sprinkles: 25,
-            marshmallows: 25,
-            sauces: {
-                choco: 50,
-                caramel: 50,
-                strawberry: 50
-            }
-        }
-    },
-    {
-        name: 'lemonade',
-        price: 1.5
-    },
-    {
-        name: 'gambling',
-        price: 2
-    }
-]
+  },
+  {
+    name: "Lemonade",
+    price: 1.5,
+  },
+  {
+    name: "Gambling",
+    price: 2,
+  },
+];
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', {
-        menu: menu
-    })
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    menu: menu,
+  });
+});
 
 app.use((req, res, next) => {
-    res.send("ERR_404_NOT_FOUND")
-})
+  res.send("ERR_404_NOT_FOUND");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
